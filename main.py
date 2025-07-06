5fcc382973e3abe8d9554730933b2ba58d58b2f4
manish <darksalterego99@gmail.com>
2025-07-07 00:51:40 +0530

    feat: added basic CRUD for posts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI 
 from datetime import datetime
-from typing import Optional
+from typing import List,Optional
 from pydantic import BaseModel
 
 app = FastAPI(
@@ -8,7 +8,8 @@ app = FastAPI(
     description="Blog API with FastAPI",
     version="0.0.1"
 )
-
+db = []
+# Root endpoints here
 @app.get("/", tags=["Root"])
 def read_root():
     return {"Welcome to Blog-API"}
@@ -20,6 +21,21 @@ class Post(BaseModel):
     published:bool=True
     created_on:datetime=datetime.now()
 
-    @app.get("/posts", tags=["Posts"])
-    def get_posts():
-        return {"data": "There will be a list of posts"}
+@app.get("/posts", tags=["Posts"])
+def get_posts():
+    return db
+
+@app.post("/posts", response_model=Post, status_code=201, tags=["Posts"])
+def create_post(post: Post):
+    db.append(post)
+    return post
+
+@app.get("/posts/{post_id}", response_model=Post, tags=["Posts"])
+def get_post(post_id: int):
+    
+    try:
+        return db[post_id]
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Post not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
